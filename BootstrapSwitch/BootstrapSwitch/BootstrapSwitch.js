5876f7d6d05442c4dc127fd0080a943b450f8d53
Nicolas Roux <nicolas.roux@aspectize.com>
2016-12-15 19:27:52 +0100

    Init

---
+++ BootstrapSwitch/BootstrapSwitch/BootstrapSwitch.js
@@ -0,0 +1,61 @@
+/* Aspectize BootstrapSwitch extension */
+
+
+Aspectize.Extend("BootstrapSwitch", {
+
+    Properties: { State: true, Size: null, Disabled: false, Readonly: false, Indeterminate: false, Inverse: false, RadioAllOff: false, OnColor: 'primary', OffColor: 'default', OnText: 'ON', OffText: 'OFF', LabelText: '&nbsp;' },
+    Events: ['OnStateChanged'],
+    Init: function (elem) {
+
+        var options = {
+            state: Aspectize.UiExtensions.GetProperty(elem, 'State'),
+            size: Aspectize.UiExtensions.GetProperty(elem, 'Size'),
+            disabled: Aspectize.UiExtensions.GetProperty(elem, 'Disabled'),
+            readonly: Aspectize.UiExtensions.GetProperty(elem, 'Readonly'),
+            indeterminate: Aspectize.UiExtensions.GetProperty(elem, 'Indeterminate'),
+            inverse: Aspectize.UiExtensions.GetProperty(elem, 'Inverse'),
+            radioAllOff: Aspectize.UiExtensions.GetProperty(elem, 'RadioAllOff'),
+            onColor: Aspectize.UiExtensions.GetProperty(elem, 'OnColor'),
+            offColor: Aspectize.UiExtensions.GetProperty(elem, 'OffColor'),
+            onText: Aspectize.UiExtensions.GetProperty(elem, 'OnText'),
+            offText: Aspectize.UiExtensions.GetProperty(elem, 'OffText'),
+            labelText: Aspectize.UiExtensions.GetProperty(elem, 'LabelText')
+        }
+
+        while (elem.firstChild) {
+            elem.removeChild(elem.firstChild);
+        }
+
+        var checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+
+        elem.appendChild(checkbox);
+
+        var theSwitch = $(checkbox);
+
+        $(checkbox).bootstrapSwitch(options);
+
+        $(checkbox).on('switchChange.bootstrapSwitch', function (event, state) {
+            //console.log(this); // DOM element
+            //console.log(event); // jQuery event
+            //console.log(state); // true | false
+
+            Aspectize.UiExtensions.ChangeProperty(elem, 'State', state);
+            Aspectize.UiExtensions.ChangeProperty(elem, 'Indeterminate', false);
+        });
+
+        Aspectize.UiExtensions.AddMergedPropertyChangeObserver(elem, function (sender, arg) {
+
+            for (var p in arg) {
+                var optionName = p.charAt(0).toLowerCase() + p.slice(1);
+
+                if (optionName == 'state') {
+                    theSwitch.bootstrapSwitch(optionName, arg[p], true);
+                } else {
+                    theSwitch.bootstrapSwitch(optionName, arg[p]);
+                }
+            }
+        });
+
+    }
+});
